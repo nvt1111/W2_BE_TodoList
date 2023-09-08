af0f0227e4f1f285cce850e8962fd3e2d5a4c78f
vanthai <nguyenvanthai03102001@gmail.com>
2023-09-08 16:26:49 +0700

    fix add todo

--- database/todoRepository.js
+++ database/todoRepository.js
@@ -4,37 +4,34 @@ const todos = readFileSync();
 
 export function getAll() {
     let result = [...todos];
+
     return result;
 }
-
 
 export function add(data) {
     const todo = { id: parseInt(Date.now()), ...data };
     const dataObject = [{ ...todo }, ...todos]
     //data: Object
     writeFileSync(dataObject);
+
     return dataObject
 }
 
 export function updateById(id) {
-    //todo: tìm cách update khác đi nhé , không nên dùng map ở chỗ này 
-    const todoList = todos.map(todo => {
-        if (todo.id === parseInt(id)) {
-            return {
-                ...todo,
-                completed: !todo.completed
-            }
-        }
-        return todo;
-    })
+    const todoIndex = todos.findIndex(todo => todo.id === parseInt(id))
+    const todo = todos.find(todo => todo.id === parseInt(id));
+    const newTodo = { ...todo, completed: !todo.completed }
+    const todoList = [...todos];
+    todoList[todoIndex] = newTodo
     writeFileSync(todoList);
+
     return todoList
 }
 
 export function deleteById(id) {
     const todoList = todos.filter(todo => todo.id !== parseInt(id));
-
     writeFileSync(todoList);
+
     return todoList;
 }
 
